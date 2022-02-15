05d6270bc81a157a56dd2219bece4b92b638afb3
starikovyaroslav <slava.com.com90@gmail.com>
2022-02-15 22:35:20 +0700

    feat: сверстать блок promo

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+import Promo from '../Promo/Promo';
+
+
+export const Main = () => {
+  return (
+    <main>
+      <Promo />
+    </main>
+  )
+}
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Promo.css'
+
+const Promo = () => {
+  return (
+    <section className="promo">
+      <div className="promo__inner">
+        <h1 className="promo__title">
+          Учебный проект студента факультета Веб-разработки.
+        </h1>
+      </div>
+      {/* <NavTab /> */}
+    </section>
+  )
+}
+
+export default Promo
