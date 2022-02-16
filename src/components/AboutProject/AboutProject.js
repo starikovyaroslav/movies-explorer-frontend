f611e3ca0838857e3040d2d843dcc9d4ff23078f
starikovyaroslav <slava.com.com90@gmail.com>
2022-02-16 23:00:13 +0700

    feat: создать компонент с описанием дипломного проекта + адаптив

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import  './AboutProject.css'
+
+const AboutProject = () => {
+  return (
+    <section className="about">
+      <div className="about__container">
+        <h2 className="about__title">О проекте</h2>
+        <div className="about__inner">
+          <div className="about__text-container">
+            <p className="about__subtitle">Дипломный проект включал 5 этапов</p>
+            <p className="about__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          </div>
+          <div className="about__text-container">
+            <p className="about__subtitle">На выполнение диплома ушло 5 недель</p>
+            <p className="about__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+          </div>
+        </div>
+        <div className="about__layer-container">
+          <div className="about__layer-inner">
+            <div className="about__layer about__layer_color_blue">1 неделя</div>
+            <p className="about__layer-text">Back-end</p>
+          </div>
+          <div className="about__layer-inner">
+            <div className="about__layer about__layer_color_gray">4 недели</div>
+            <p className="about__layer-text">Front-end</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutProject
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Main.css';
 import Promo from '../Promo/Promo';
-
+import AboutProject from '../AboutProject/AboutProject';
 
 export const Main = () => {
   return (
     <main>
       <Promo />
+      <AboutProject />
     </main>
   )
 }
