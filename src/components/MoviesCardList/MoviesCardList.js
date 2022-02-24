bcc17c2eef05dcfa7eeb5da2c64939f337d2fed3
starikovyaroslav <slava.com.com90@gmail.com>
2022-02-24 20:51:04 +0700

    feat: добавить кнопку "еще" + адаптив

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,20 +4,25 @@ import './MoviesCardList.css';
 
 const MoviesCardList = () => {
   return (
-    <section className="moviesCard">
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-    </section>
+    <>
+      <section className="movies-list">
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+        <MoviesCard/>
+      </section>
+      <div className="movies-list__more">
+        <button className="movies-list__button">Еще</button>
+      </div>
+    </>
   );
 }
 
