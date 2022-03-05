b7cf8531477af448cfb78703db208d1ddf30c25e
starikovyaroslav <slava.com.com90@gmail.com>
2022-03-05 23:55:49 +0700

    feat: реализовать подгрузку карточек на главную страницу

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -2,16 +2,21 @@ import React from "react";
 import moviePhoto from '../../images/photo.png';
 import './MoviesCard.css';
 
-const MoviesCard = () => {
+const MoviesCard = ({movie}) => {
+
+  const getTime = () => {
+    return `${Math.floor(movie.duration / 60)}ч ${movie.duration % 60}м`;
+  }
+
   return (
     <li className="movie">
       <div className="movie__container">
         <button className='movie__button movie__button-save' type='button'>Сохранить</button>
-        <img className="movie__image" src={moviePhoto} alt="Название фильма"/>
+        <img className="movie__image" src={`https://api.nomoreparties.co/${movie.image.url}`} alt="Название фильма"/>
       </div>
       <div className="movie__text-box">
-        <h2 className="movie__title">Название</h2>
-        <p className='movie__subtitle'>Время</p>
+        <h2 className="movie__title">{movie.nameRU}</h2>
+        <p className='movie__subtitle'>{getTime()}</p>
       </div>
     </li>
   );
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,29 +1,88 @@
 import React from "react";
+
+import { useLocation } from 'react-router';
 import MoviesCard from "../MoviesCard/MoviesCard";
-import './MoviesCardList.css';
+import Preloader from "../Preloader/Preloader";
+import "./MoviesCardList.css";
+import api from "../../utils/MoviesApi";
 
 const MoviesCardList = () => {
+
+  const location = useLocation();
+  const [size, setSize] = React.useState(0);
+  const [moviesList, setMoviesList] = React.useState([]);
+  const [moviesTotal, setMoviesTotal] = React.useState(0);
+  const [addMovies, setAddMovies] = React.useState(0);
+
+  function resizeHandler() {
+    setSize(window.innerWidth);
+  }
+
+  function getCards() {
+    if (size >= 1280) {
+      setMoviesTotal(12);
+      setAddMovies(3);
+    } else if (size < 1280 && size > 768) {
+      setMoviesTotal(8);
+      setAddMovies(2);
+    } else if (size < 767) {
+      setMoviesTotal(5);
+      setAddMovies(2);
+    }
+  }
+
+  function addCards() {
+    setMoviesTotal(moviesTotal + addMovies);
+  }
+
+  React.useEffect(() => {
+    getCards();
+  }, [size]);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    api
+      .getInitialCards()
+      .then((cards) => {
+        setMoviesList(cards);
+      })
+      .catch((err) => {
+        console.log(`Внимание! ${err}`);
+      });
+  }, []);
+
+
   return (
     <>
-      <section className="movies-list">
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
-        <MoviesCard/>
+      <section>
+        { moviesList.length === 0 ? (
+          <Preloader />
+        ) : (
+          <ul className="movies-list">
+          { moviesList.map((movie, id) => {
+            if (id + 1 <= moviesTotal) {
+              return <MoviesCard movie={movie} key={id} />;
+            } else {
+              return '';
+            }
+            })}
+          </ul>
+        )}
       </section>
-      <div className="movies-list__more">
-        <button className="movies-list__button">Еще</button>
-      </div>
+      { moviesTotal < moviesList.length && location.pathname === '/movies' && (
+        <div className="movies-list__more">
+          <button className="movies-list__button" onClick={addCards}>Еще</button>
+        </div>
+      )}
     </>
   );
-}
+};
 
 export default MoviesCardList;
